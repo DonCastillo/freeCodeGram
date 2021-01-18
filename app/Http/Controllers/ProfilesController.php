4d2ce1c7c58fbbda15f8b31a2fa10b26891a2a89
DonCastillo <don.qcastillo@yahoo.com>
2021-01-17 20:40:02 -0700

    added follow button

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -45,14 +45,16 @@ class ProfilesController extends Controller
             $imagePath = request('image')->store('profile', 'public');
             $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000, 1000);
             $image->save();
+            $imageArray = ['image' => $imagePath];
         }
 
 
         # grabbing only the authenticated user
         # extra layer of protection
+
         auth()->user()->profile->update(array_merge(
             $data,
-            ['image' => $imagePath]
+            $imageArray ?? []
         ));
 
         return redirect("profile/{$user->id}");
